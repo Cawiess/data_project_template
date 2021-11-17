cd3df5b16a8a2cad1e3b3701a75b0871a8ad3344
Cawiess <carl.wictor.essman@gmail.com>
2021-11-17 09:16:45 +0200

    connecting and running load data module

--- {{cookiecutter.project_name}}/packages/{{cookiecutter.package_name}}/{{cookiecutter.package_name}}/main.py
+++ {{cookiecutter.project_name}}/packages/{{cookiecutter.package_name}}/{{cookiecutter.package_name}}/main.py
@@ -1,7 +1,13 @@
+import pandas as pd
+
+from {{cookiecutter.package_name}}.config.config import config
 from {{cookiecutter.package_name}}.processing.data_manager import DataManager
 
 def main():
     print('Starting...') # TODO: LOG HERE
+    raw_data = DataManager.load_csv(config.package_config.raw_data)
+    print(raw_data.columns)
+
 
     return 0
 
