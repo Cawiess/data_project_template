69752a71f844e8c94d14da2a4a833a5bd0b15046
Cawiess <carl.wictor.essman@gmail.com>
2021-11-17 13:39:03 +0200

    add versioning

--- {{cookiecutter.project_name}}/packages/{{cookiecutter.package_name}}/{{cookiecutter.package_name}}/processing/data_manager.py
+++ {{cookiecutter.project_name}}/packages/{{cookiecutter.package_name}}/{{cookiecutter.package_name}}/processing/data_manager.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 from {{cookiecutter.package_name}}.config import config
+from {{cookiecutter.package_name}} import __version__ as _version
 
 
 class DataManager:
@@ -9,7 +10,7 @@ class DataManager:
         return _data
 
     def export_excel(*, data: pd.DataFrame, file_name: str) -> None:
-        data.to_excel(f"{config.OUTPUT_DIR}/{file_name}_{config.TODAY}.xlsx", index=False)
+        data.to_excel(f"{config.OUTPUT_DIR}/{file_name}_v_{_version}_{config.TODAY}.xlsx", index=False)
         return None
 
 
